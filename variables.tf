32dc29e194216aad74ab0cd7dd8db4c684ef3b69
Prathamesh Warekar <warekarprathamesh@gmail.com>
2025-05-12 15:47:37 +0100

    Revert "Update variables.tf"
    This reverts commit ccd4b0f3a191fe7bcfe38a3a97165dd2363ebc21.

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "vpc_cidr" { default = "10.0.0.0/16" }
 variable "instance_type" { default = "t2.micro" }
 variable "ami_id" { default = "ami-0c02fb55956c7d316" } # Amazon Linux 2
-variable "azs" { default = ["eu-west-1", "eu-west-2"] }
+variable "azs" { default = ["us-east-1a", "us-east-1b"] }
 
